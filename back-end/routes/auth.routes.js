2129116d3363a94ac193d61d405249e6325b69a9
Castard Allan <castard75@gmail.com>
2022-04-19 10:52:08 +0200

    email validation

--- back-end/routes/auth.routes.js
+++ back-end/routes/auth.routes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 const passwordControl = require("../middleware/passwordValidations");
+const validationEmail = require("../middleware/emailToValidate");
 
-router.post("/signup", passwordControl.Password, authController.signup);
+router.post(
+  "/signup",
+  passwordControl.Password,
+  validationEmail.emailValidation,
+  authController.signup
+);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/desactivateAccount/:id", authController.desactivateAccount);
